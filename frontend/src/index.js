183ab9494a0a353b3783355eae7164752ac4826a
FooChuanTian <foochuantian@gmail.com>
2024-11-10 11:14:24 +0800

    added more links and changed title

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <title>Chuan Tian's Portfolio</title>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
